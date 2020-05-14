3b3bd5b4b642422cb3d5bd48dbe9b555e4145371
Sua Le <sua8051@gmail.com>
2020-05-14 10:51:35 +0700

    - update server url

--- src/main/java/de/blau/android/contract/Urls.java
+++ src/main/java/de/blau/android/contract/Urls.java
@@ -12,14 +12,14 @@ public final class Urls {
         // empty
     }
 
-    public static final String DEFAULT_API               = "https://api.openstreetmap.org/api/0.6/";
-    public static final String DEFAULT_API_NAME          = "OpenStreetMap";
-    public static final String DEFAULT_API_NO_HTTPS      = "http://api.openstreetmap.org/api/0.6/";
-    public static final String DEFAULT_API_NO_HTTPS_NAME = "OpenStreetMap no https";
-    public static final String DEFAULT_SANDBOX_API       = "https://master.apis.dev.openstreetmap.org/api/0.6/";
-    public static final String DEFAULT_SANDBOX_API_NAME  = "OpenStreetMap sandbox";
+    public static final String DEFAULT_API               = "https://its.vimap.vn/api/0.6/";
+    public static final String DEFAULT_API_NAME          = "ITS Editor";
+    public static final String DEFAULT_API_NO_HTTPS      = "http://its.vimap.vn/api/0.6/";
+    public static final String DEFAULT_API_NO_HTTPS_NAME = "ITS Editor Http";
+    public static final String DEFAULT_SANDBOX_API       = "https://its.vimap.vn/api/0.6/";
+    public static final String DEFAULT_SANDBOX_API_NAME  = "ITSEditor sandbox";
 
-    public static final String DEFAULT_NOMINATIM_SERVER = "https://nominatim.openstreetmap.org/";
+    public static final String DEFAULT_NOMINATIM_SERVER = "https://nano.map4d.vn/";
     public static final String DEFAULT_PHOTON_SERVER    = "https://photon.komoot.de/";
 
     public static final String DEFAULT_OSMOSE_SERVER      = "https://osmose.openstreetmap.fr/";
@@ -33,8 +33,8 @@ public final class Urls {
     public static final String WIKIPEDIA = "https://en.wikipedia.org/wiki/";
     public static final String WIKIDATA  = "https://wikidata.org/wiki/";
 
-    public static final String OSM       = "https://openstreetmap.org";
-    public static final String OSM_LOGIN = "https://www.openstreetmap.org/login";
+    public static final String OSM       = "https://its.vimap.vn";
+    public static final String OSM_LOGIN = "https://its.vimap.vn/login";
     public static final String OSM_WIKI  = "https://wiki.openstreetmap.org/";
 
     public static final String JOSM_IMAGERY = "https://josm.openstreetmap.de/maps?format=geojson";
